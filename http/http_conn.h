2e478a070b2be1b85202d2f18faf8e3b5b8a0be6
pccao0912 <pccao@ucdavis.edu>
2023-04-18 20:53:47 -0700

    Update

--- http/http_conn.h
+++ http/http_conn.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef HTTPCONNECTION_H
 #define HTTPCONNECTION_H
 
@@ -28,35 +27,4 @@ class http_conn {
         static const int Filename_len = 200;
         static const int Read_buffer_size = 2048;
         static const int Write_buffer_size = 1024;
-=======
-#ifndef HTTPCONNECTION_H
-#define HTTPCONNECTION_H
-
-#include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/epoll.h>
-#include <fcntl.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <assert.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <stdarg.h>
-#include <errno.h>
-#include <sys/wait.h>
-#include <sys/uio.h>
-#include <map>
-
-class http_conn {
-    public:
-        static const int Filename_len = 200;
-        static const int Read_buffer_size = 2048;
-        static const int Write_buffer_size = 1024;
->>>>>>> c2cc551c1195f9a2b65d7671ae224b5a53bd186c
 };
