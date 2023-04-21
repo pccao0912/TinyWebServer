c8e3ed2870fa307c4fea9b1d86da615b2c4e2ac0
pccao0912 <pccao0912@gmail.com>
2023-04-20 18:52:22 -0700

    4/20 update

---
+++ log/block_queue.h
@@ -0,0 +1,33 @@
+#ifndef BLOCK_QUEUE_H
+#define BLOCK_QUEUE_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include "../lock/locker.h"
+
+using namespace std;
+
+template<class T>
+
+class block_queue {
+private:
+    locker m_mutex;
+    cond m_cond;
+
+    T *m_array;
+    int m_size;
+    int m_max_size;
+    int m_front;
+    int m_back;
+
+public:
+    block_queue(int max_size = 1000) {
+        if (max_size <= 0) {
+            exit(1);
+        }
+    }
+};
+
+#endif
